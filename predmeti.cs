bac939deb7fbfb75b32145d34625874ee0233679
milos pusic <milos.pusic@prvabeogim.edu.rs>
2025-05-28 12:42:06 +0200

    Grid1

---
+++ predmeti.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EDnevnik2024A
+{
+    public partial class predmeti : Form
+    {
+        public predmeti()
+        {
+            InitializeComponent();
+        }
+    }
+}
